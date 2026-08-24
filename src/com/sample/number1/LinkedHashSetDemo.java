2de3ce991cc88d7e147d2d1210cab47e4d47bc2f
ashutoshmallik610 <157103768+ashutoshmallik610@users.noreply.github.com>
2026-08-24 16:46:21 +0530

    Sample

---
+++ src/com/sample/number1/LinkedHashSetDemo.java
@@ -0,0 +1,24 @@
+package com.sample.number1;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+
+    public static void main(String[] args) {
+
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        set.add(40);
+        set.add(50);
+        set.add(60);
+        set.add(70);
+        set.add(80);
+        set.add(90);
+        set.add(100);
+
+        System.out.println(set);
+    }
+}
